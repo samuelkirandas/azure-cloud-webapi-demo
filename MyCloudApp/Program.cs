e62835eecebe2c05512cbb84469b4822abbadf91
Samuel Kiran Das <samuelkiran.d@utthunga.com>
2025-11-10 18:12:53 +0530

    Test changes messages

--- MyCloudApp/Program.cs
+++ MyCloudApp/Program.cs
@@ -36,7 +36,7 @@ app.MapGet("/weatherforecast", () =>
 .WithName("GetWeatherForecast")
 .WithOpenApi();
 
-app.MapGet("/health/live", () => Results.Ok("Alive"));
+app.MapGet("/health/live", () => Results.Ok("Haha Alive"));
 app.MapGet("/health/ready", () => Results.Ok("Ready"));
 
 app.Run();
